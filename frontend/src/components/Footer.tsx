3031f2b39458ce80dd972f7fc3b9a342d43d1c57
ridho <ridloghfry@gmail.com>
2024-02-20 20:01:12 +0700

    Fix header & hero section styling

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,12 +2,16 @@ const Footer = () => {
   return (
     <div className="bg-blue-700 py-10 p-6">
       <div className="container mx-auto flex justify-between items-center">
-        <span className="text-3xl text-white font-bold tracking-tight">
+        <span className="text-xl md:text-2xl lg:text-3xl text-white font-bold tracking-tight">
           Holidayuks
         </span>
         <span className="text-white font-bold tracking-tight flex gap-4">
-          <p className="cursor-pointer">Privacy Policy</p>
-          <p className="cursor-pointer">Terms of Service</p>
+          <p className="cursor-pointer text-xs md:text-md lg:text-lg">
+            Privacy Policy
+          </p>
+          <p className="cursor-pointer text-xs md:text-md lg:text-lg">
+            Terms of Service
+          </p>
         </span>
       </div>
     </div>
--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -2,10 +2,10 @@ const Hero = () => {
   return (
     <div className="bg-blue-700 pb-16 p-6">
       <div className="container mx-auto flex flex-col gap-2">
-        <h1 className="text-3xl md:text-4xl lg:text-5xl text-white font-bold">
+        <h1 className="text-2xl md:text-3xl lg:text-4xl text-white font-bold">
           Find your next stay
         </h1>
-        <p className="text-lg md:text-xl lg:text-2xl text-white">
+        <p className="text-md md:text-lg lg:text-xl text-white">
           Search low prices on hotels for your dream vacation...
         </p>
       </div>
