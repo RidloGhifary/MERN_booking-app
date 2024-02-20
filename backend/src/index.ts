8f6356dbf7d85a8f298a188149da4140efebc177
ridho <ridloghfry@gmail.com>
2024-02-20 11:53:33 +0700

    MongoDB connec & create User model & create router

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,22 @@
 import express,{Request, Response} from "express"
 import cors from "cors"
 import "dotenv/config"
+import mongoose from 'mongoose';
+
+import userRoutes from "./router/users"
 
 const app = express()
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors())
 
+app.use("/api/users", userRoutes)
+
 app.get("/api/test",async(req:Request,res:Response)=>{
-res.json({message:"Hello from backend"})
+  res.json({message:"Hello from backend"})
 })
 
 app.listen(5000, ()=>{console.log("Connected...")})
