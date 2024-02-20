e07bdc5712129afcbfda68f751914a4feb0b1a8a
ridho <ridloghfry@gmail.com>
2024-02-20 13:47:47 +0700

    Login authentication

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import "dotenv/config";
 import mongoose from "mongoose";
 
 import userRoutes from "./router/users";
+import authRoutes from "./router/auth";
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
 app.get("/api/test", async (req: Request, res: Response) => {
