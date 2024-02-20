03ef85b815f3e1ee185097b41716d4b935f9cdaf
ridho <ridloghfry@gmail.com>
2024-02-20 13:50:01 +0700

    Fix response error

--- backend/src/router/auth.ts
+++ backend/src/router/auth.ts
@@ -31,7 +31,7 @@ router.post(
       const isMatch = await bcrypt.compare(password, user.password);
 
       if (!isMatch)
-        return res.send(400).json({ message: "Invalid credentials" });
+        return res.status(400).json({ message: "Invalid credentials" });
 
       const token = jwt.sign(
         { userId: user.id },
