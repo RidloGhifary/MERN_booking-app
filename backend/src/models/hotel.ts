e1ba39825014ef96b11627a5b7642e44c5d3ef21
ridho <ridloghfry@gmail.com>
2024-02-21 20:11:07 +0700

    Create Hotel model

---
+++ backend/src/models/hotel.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+export type HotelType = {
+  _id: string;
+  userId: string;
+  name: string;
+  city: string;
+  description: string;
+  type: string;
+  adultCount: number;
+  childCount: number;
+  facilities: string[];
+  pricePerNIght: number;
+  starRating: number;
+  imageUrls: string[];
+  lastUpdated: Date;
+};
+
+const hotelSchema = new mongoose.Schema({
+  userId: { type: String, require: true },
+  name: { type: String, require: true },
+  city: { type: String, require: true },
+  description: { type: String, require: true },
+  type: { type: String, require: true },
+  adultCount: { type: Number, require: true },
+  childCount: { type: Number, require: true },
+  facilities: [{ type: String, required: true }],
+  pricePerNIght: { type: Number, require: true },
+  starRating: { type: Number, require: true, min: 1, max: 5 },
+  imageUrls: [{ type: String, required: true }],
+  lastUpdated: { type: Date, require: true },
+});
+
+const Hotel = mongoose.model<HotelType>("Hotel", hotelSchema);
+export default Hotel;
