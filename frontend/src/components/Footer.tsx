63217db4269d30c3ed34675c455eaaf163ddab4a
ridho <ridloghfry@gmail.com>
2024-02-25 21:41:51 +0700

    Fix footer styling

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="bg-blue-700 py-10 p-6">
+    <div className="bg-blue-700 p-6">
       <div className="container mx-auto flex justify-between items-center">
         <span className="text-xl md:text-2xl lg:text-3xl text-white font-bold tracking-tight">
           Holidayuks
